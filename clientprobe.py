89ab31fed30c2008c437eaeb8ac101ca59443322
Tyler <tyler@zonksec.com>
2018-03-25 14:15:00 -0500

    changed naming. changed to better OUI package

--- clientprobe.py
+++ clientprobe.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 from scapy.all import *
-import netaddr
 import sys
 import time
 import operator
 from termcolor import colored
+from manuf import manuf
 
-probes = []
+clients = []
 
 def packet_handler(pkt):
     #grab new client probe
@@ -16,62 +16,50 @@ def packet_handler(pkt):
            mac = pkt.addr2
            strength = str(-(256-ord(pkt.notdecoded[-4:-3])))
            seen = False
-           for probe in probes:
+           for client in clients:
            #if already seen client, update count, strength, time, and add ssid if not seen before
-               if probe.clientMAC == mac:
-                   probe.count = probe.count + 1
-                   probe.strength = strength
-                   probe.lastSeen = lastSeen=time.time()
+               if client.clientMAC == mac:
+                   client.count = client.count + 1
+                   client.strength = strength
+                   client.lastSeen = lastSeen=time.time()
                    seen=True
-                   if pkt[Dot11ProbeReq].info not in probe.ssid:
-                       probe.ssid.append(pkt[Dot11ProbeReq].info)
+                   if pkt[Dot11ProbeReq].info not in client.ssid:
+                       client.ssid.append(pkt[Dot11ProbeReq].info)
            #else add the new client and ssid
            if not seen:
-               probe = probeEntry(clientMAC=pkt.addr2,count=1,lastSeen=time.time(),strength=strength)
-               probe.ssid.append(pkt[Dot11ProbeReq].info)
-               probes.append(probe)
+               client = clientEntry(clientMAC=pkt.addr2,count=1,lastSeen=time.time(),strength=strength)
+               client.ssid.append(pkt[Dot11ProbeReq].info)
+               clients.append(client)
 
            #clear screen and print ascii
            print(chr(27) + "[2J")
            printAscii()
            
-           #sort probes so newest on top. limit limit lines printed to screen. print data
+           #sort clients so newest on top. limit limit lines printed to screen. print data
            lineCount = 0
-           probes.sort(key=operator.attrgetter('lastSeen'),reverse=True)
-           for probe in probes:
+           clients.sort(key=operator.attrgetter('lastSeen'),reverse=True)
+           for client in clients:
                if not lineCount > 25: 
                    lineCount = lineCount + 2
-                   print colored("Client MAC: ","red")+probe.clientMAC+colored(" Count: ","red")+'{:3}'.format(probe.count) + colored(" dBm: ","red") + probe.strength + colored(" OUI: ","red") + probe.oui
-                   for ssid in probe.ssid:
+                   print colored("Client MAC: ","red")+client.clientMAC+colored(" Count: ","red")+'{:3}'.format(client.count) + colored(" dBm: ","red") + client.strength + colored(" OUI: ","red") + client.oui
+                   for ssid in client.ssid:
                        print colored(ssid,"yellow")
                        lineCount = lineCount + 1
                    print
 
 
-class probeEntry:
+class clientEntry:
     def __init__(self,clientMAC,count,lastSeen,strength):
         self.clientMAC = clientMAC
         self.ssid = []
         self.count = count
         self.lastSeen = lastSeen
         self.strength = strength
-
-        try:
-            parsed_mac = netaddr.EUI(clientMAC)
-            self.oui = parsed_mac.oui.registration().org
-            if self.oui == "":
-                self.out = "Unknown"
-        except netaddr.core.NotRegisteredError, e:
-            self.oui = "Unknown"
+        self.oui = str(p.get_manuf_long(str(clientMAC)))
 
 
 def printAscii():
-<<<<<<< HEAD
     print colored("""
-=======
-    print """
->>>>>>> da62cea73e82aa92e3eb78fc14e38fdbb216b458
-
 $$$$$$$\   $$$$$$\  $$\   $$\  $$$$$$\   $$$$$$\  
 $$  __$$\ $$  __$$\ $$ |  $$ |$$$ __$$\ $$  __$$\ 
 $$ |  $$ |$$ /  \__|$$ |  $$ |$$$$\ $$ |\__/  $$ |
@@ -82,7 +70,10 @@ $$$$$$$  |\$$$$$$  |      $$ |\$$$$$$  /$$$$$$$$\
 \_______/  \______/       \__| \______/ \________|
                                                                                                  
 """,'green')
+
 print(chr(27) + "[2J")
 printAscii()
+print "Loading OUI..."
+p = manuf.MacParser(update=False) #set to true to download new OUI
 print "No probe requests seen yet!"
 sniff(iface = sys.argv[1], count=0, store=0, prn=packet_handler)
